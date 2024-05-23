b2eb123d3a5815218f64c77ca94935efbddc58ae
Hydrogen suso <162978353+hydrogencum102@users.noreply.github.com>
2024-05-24 00:06:16 +0900

    muyouseung
    sadf

---
+++ Assets/00.Scripts/JYD/GrabableObject.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class GrabableObject : MonoBehaviour
+{
+    public bool isInteraction;
+
+    public MeshRenderer Renderer;
+    private Material material;
+    
+    
+    private void Start()
+    {
+        material = Renderer.material;
+        
+        
+    }
+    
+    public void EnterInteraction()
+    {
+        isInteraction = true;
+        material.SetColor("_EmissionColor",Color.green * 1f);
+
+    }
+    public void ExitInteraction()
+    {
+        isInteraction = false;
+        material.SetColor("_EmissionColor",Color.green  * 0);
+    }
+
+}
